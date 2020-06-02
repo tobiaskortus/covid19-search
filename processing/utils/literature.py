f47ccb1807c137ba4cb6db6d248e630b27f8c518
Tobias Kortus <tobias.kortus@outlook.de>
2020-06-02 18:06:05 +0200

    added functionality to extract authors with associated institutions + bibliographic references from json (+ additional plausibility checks)

--- processing/utils/literature.py
+++ processing/utils/literature.py
@@ -1,15 +1,18 @@
 import json
+from zipfile import ZipFile
 import pycld2 as cld2
-from os import getcwd, pardir
-from os import listdir
+from os import getcwd, pardir, environ
+from os import listdir, mkdir
 from os.path import isfile, join
 from glob import glob
+from .grid.grid import GridLookup
+from os.path import isdir, isfile
 
 class DataLoader:
-    def __init__(self, path):
-       with open(path) as f:
+    def __init__(self, path, grid_lookup=None):
+        self.grid_lookup = grid_lookup if grid_lookup != None else GridLookup()
+        with open(path) as f:
             self.json = json.load(f)
-
 
     def get_full_text(self):
         return f"{self.get_title()} {self.get_text('abstract')} {self.get_text('body_text')}"
@@ -25,8 +28,82 @@ class DataLoader:
     def get_title(self):
         return self.json['metadata']['title']
 
-    def get_authors(self):
-        return [f"{author['first']} {''.join(author['middle'])}{' ' if len(author['middle']) != 0 else ''}{author['last']}" for author in self.json['metadata']['authors']]
+    def __is_name(self, first_name, middle_name, last_name):
+        return not len(last_name) < 2 or first_name.isalpha() or first_name is ' '
+
+    def __clean_name_part(self, x):
+        """
+        removes error in names (e.g ; Name or Name§, etc.)
+        """
+        return''.join(ch for ch in x if ch.isalpha() or ch is '-')
+
+    def __parse_author(self, raw_author, clean_names=False, plausibility_check=False):
+        if clean_names:
+            middle_names_cleaned = ''.join(self.__clean_name_part(raw_author['middle']))
+            first_name = f"{self.__clean_name_part(raw_author['first'])} "
+            middle_name = f"{middle_names_cleaned}{' ' if len(middle_names_cleaned) != 0 else ''}"
+            last_name = self.__clean_name_part(raw_author['last'])
+        else:
+            first_name = f"{raw_author['first']} "
+            middle_name = f"{''.join(raw_author['middle'])}{' ' if len(raw_author['middle']) != 0 else ''}"
+            last_name = raw_author['last']
+
+        is_name = True
+        if plausibility_check:
+            is_name = self.__is_name(first_name, middle_name, last_name)
+
+        return f"{first_name}{middle_name}{last_name}" if is_name else None
+
+    def __parse_institution(self, institution_raw, plausibility_check=False):
+        """
+        TODO: add additional information from grid (e.g. Location of institution)
+        """
+        institution = institution_raw
+
+        if plausibility_check:
+                is_institution = self.grid_lookup.get_institution(institution) != None
+                institution = institution if is_institution else None
+
+        return institution
+
+
+    def get_authors(self, plausibility_check=True, clean_names=True):
+        authors = []
+
+        for author in self.json['metadata']['authors']:
+
+            institution = self.__parse_institution(
+                institution_raw=author['affiliation'].get('institution'),
+                plausibility_check=plausibility_check)
+
+            author = self.__parse_author(
+                raw_author=author,
+                clean_names=clean_names, 
+                plausibility_check=plausibility_check)
+            
+            authors.append((author, institution))
+
+        return authors
+
+
+    def get_bib_entries(self):
+        ref_entries = []
+        index = 1
+        ref_entry = self.json['bib_entries'].get(f'BIBREF{index}')
+        while ref_entry is not None:
+            index+=1
+            title = ref_entry['title']
+            authors = [self.__parse_author(a, False, True) for a in ref_entry['authors']]
+            ref_entries.append((title, authors, ref_entry))
+            ref_entry = self.json['bib_entries'].get(f'BIBREF{index}')
+
+        return ref_entries
+
+    def get_raw_json(self):
+        return self.json
+
+    def get_paper_id(self):
+        return self.json['paper_id']
 
 
 def get_files(root_dir):
@@ -63,6 +140,10 @@ def get_authors(fpath, dl=None):
     if dl is None: dl = DataLoader(fpath)
     return dl.get_authors()
 
+def get_ref_entires(fpath, dl=None):
+    if dl is None: dl = DataLoader(fpath)
+    return dl.get_ref_entires()
+
 def is_english(text):
     is_reliable, _, details = cld2.detect(text)
     if not is_reliable or details[0][1] != 'en':
@@ -81,8 +162,4 @@ def get_section(fpath, section, dl=None):
             
     return text
 
-def get_sections():
-    return {
-        'title': 0,
-        'abstract': 1,
-        'body_text': 2 }
+def get_sections(): return {'title': 0, 'abstract': 1, 'body_text': 2 }
