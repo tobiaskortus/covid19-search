e0cc2cc952ea361553a26cd149ccce5ef0246cc9
Tobias Kortus <tobias.kortus@outlook.de>
2020-06-10 14:04:16 +0200

    fixed an issue where an error was thrown when sumitting the search term with the search button + minor changes in frontend ui & code

--- web_application/frontend/src/components/Header.js
+++ web_application/frontend/src/components/Header.js
@@ -1,8 +1,14 @@
 import './Header.css'
 import search from '../assets/icons/search-white-18dp.svg'
+import clear from '../assets/icons/clear-white-18dp.svg'
+
 import Particles from 'react-particles-js';
 import React, { Component } from 'react'
 import { Image } from 'react-bootstrap'
+import { Tooltip } from '@material-ui/core'
+
+
+const ENTER = 13;
 
 //TODO: Make header reactive 
 export class Header extends Component {
@@ -10,45 +16,61 @@ export class Header extends Component {
     constructor() {
         super()
         this.state = { searchTerm: "" }
+        this.particleParams = {
+            "particles": {
+                "number": {
+                    "value": 150,
+                    "density": {
+                        "enable": true,
+                        "value_area": 1803.4120608655228
+                    }
+                }
+            }
+        };
     }
 
     handleChange = (e) => this.setState({searchTerm: e.target.value})
 
     handleKeyDown = (e) => {
-        if (e.keyCode === 13) {
+        if (e.keyCode === ENTER) {
             this.props.submit(this.state.searchTerm)
         }
     }
 
-    //<Image src={background} style={{zIndex: '-1', height: '100%', width: '100%'}}/>//
-    //<VirusIcon className='app-icon'/>
-
     render() {
         return (
             <div className='header-collapsed'>
-               <Particles params={{"particles": {
-                    "number": {
-                        "value": 150,
-                        "density": {
-                            "enable": true,
-                            "value_area": 1803.4120608655228
-                        }
-                    }}}}/>
-                <div className='topLeft'>
+               <Particles params={this.particleParams}/>
+                    
+                <div className='top-left'>
                     <div style={{display: 'flex'}}>
                         <div style={{paddingLeft: '0px'}}>
-                            <h1 className='title'>Covid-19 Insights</h1>
+                            <h1 style={{fontSize: '40px'}} className='title'>Covid Open Research Engine</h1>
                             <h6 className='title'>A search engine for covid-19 literature research</h6>
                         </div>
                     </div>
-                    <input  className='custom-search-field' 
-                            type='text' 
-                            placeholder='What are you looking for ?'
-                            value={this.state.searchTerm} 
-                            onChange={this.handleChange}
-                            onKeyDown={this.handleKeyDown} />
+
+                    <div className='input-container'>
+                        <input  className='transparent-search-field' 
+                                type='text' 
+                                placeholder='What are you looking for ?'
+                                value={this.state.searchTerm} 
+                                onChange={this.handleChange}
+                                onKeyDown={this.handleKeyDown} />
                         
-                    <Image onClick={() => this.props.submit(this.state.searchTerm.bind(this))} src={search} className='search-icon'/>
+                        <div className='icon-container'>
+                            {
+                                this.state.searchTerm !== '' &&
+                                <Tooltip title='clear search input'>
+                                <Image onClick={() => this.setState({searchTerm: ''})} src={clear} className='search-icon'/>
+                                </Tooltip>
+                            }
+                    
+                            <Tooltip title='search'>
+                                <Image onClick={() => this.props.submit(this.state.searchTerm)} src={search} className='search-icon'/>
+                            </Tooltip>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
